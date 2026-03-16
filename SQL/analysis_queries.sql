8bcf0a11e789030e802ffc5ff39bd1d21754c0bd
emilostrowski16 <emil.ostrowski.16@gmail.com>
2026-03-16 19:56:27 +0100

    Update analysis_queries.sql

--- SQL/analysis_queries.sql
+++ SQL/analysis_queries.sql
@@ -11,3 +11,19 @@ FROM olist_order_payments_dataset;
 -- Average order value
 SELECT SUM(payment_value) / COUNT(DISTINCT order_id) AS average_order_value
 FROM olist_order_payments_dataset;
+
+-- Number of orders per month
+SELECT STRFTIME('%m', order_purchase_timestamp) AS month,
+       COUNT(order_id) AS total_orders
+FROM olist_orders_dataset
+GROUP BY month
+ORDER BY month;
+
+-- Revenue per month
+SELECT STRFTIME('%Y-%m', od.order_purchase_timestamp) AS date,
+       SUM(opd.payment_value) AS total_revenue
+FROM olist_orders_dataset AS od
+JOIN olist_order_payments_dataset AS opd
+ON od.order_id = opd.order_id
+GROUP BY date
+ORDER BY date;
